858c60d3b03cc9f4161d0c2e994b990ef68f31bc
S2371502 <s2371502@let-edu-145-65.let.rug.nl>
2016-05-04 15:56:52 +0200

    seminar update 1

--- 3.py
+++ 3.py
@@ -3,18 +3,43 @@
 import sys
 from SPARQLWrapper import SPARQLWrapper, JSON
 
+lidwoorden = ["de", "het", "een"]
+
 def print_example_queries():
 	print("Wat is de startdatum van de Olympische Spelen 2012?")
 	print("Wat is de plaats van de Olympische Zomerspelen 2008?")
 	print("Wat is de lengte van Usain Bolt?")
 	print("Wie is de trainer van Usain Bolt?")
 
-def find_concept(concept):
-	for line in open("pairCounts"):
-		if concept in line:
-			line = line.split('\t')
-			concept_Y = (line[1])
-			return concept_Y
+def create_and_fire_query(line):
+	propX = find_prop(line)
+	conceptY = find_concept(line)
+
+def find_prop(line):
+	line = line.split()
+	if line[2] in lidwoorden:
+		propX = line[3]
+	else: 
+		propX = line[2]
+
+	return(propX)
+
+
+def find_concept(line):
+	#make concept usable to find in paircounts
+	concept = line.split("van")[1]
+	concept = concept.replace("?", "")
+	concept = concept.replace("de", "")
+	concept = concept.replace("het", "")
+	concept = concept.strip()
+	print(concept)
+
+	for pair in open("pairCounts"):
+		if str(concept) in pair:
+			print(pair)
+			pair = pair.split('\t')
+			concept_Y = (pair[1])
+			print(concept_Y)
 
 #def create_and_fire_query(line):
 
---
+++ grep.py
@@ -0,0 +1,6 @@
+def main():
+	for pair in open("pairCounts"):
+		if "Usain Bolt" in pair:
+			print(pair)
+
+main()
